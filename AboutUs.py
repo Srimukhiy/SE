7250701b2a0f8720d2b375fb50e739ff922d8575
ShashankaGifts <60562385+ShashankaGifts@users.noreply.github.com>
2023-10-22 20:00:32 -0500

    Software Engineering Project_6_Requirements

---
+++ AboutUs.py
@@ -0,0 +1,8 @@
+import streamlit as st;
+def app():
+    st.subheader('This website is used to understand the data by visually mpaired people ')
+   
+    st.markdown('Created by: [ShashankaEega](https://github.com/EegaShashanka)')
+    st.markdown('Contact via mail: [shashankaEega@gmail.com]')
+
+    
