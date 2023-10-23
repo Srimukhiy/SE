22bab49f1f40bb74f98c74f983be20018dac0ca2
ShashankaGifts <60562385+ShashankaGifts@users.noreply.github.com>
2023-10-22 23:32:20 -0500

    Update AboutUs.py

--- AboutUs.py
+++ AboutUs.py
@@ -2,7 +2,7 @@ import streamlit as st;
 def app():
     st.subheader('This website is used to understand the data by visually mpaired people ')
    
-    st.markdown('Created by: [ShashankaEega](https://github.com/EegaShashanka)')
+    st.markdown('Created by: [AI – Vision verse](https://github.com/ShashankaGifts/SE)')
     st.markdown('Contact via mail: [shashankaEega@gmail.com]')
 
     
